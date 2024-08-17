73181408fce07280a458a1f3a6b9214fd25d0399
surya <122369945+suryaprasadyadav@users.noreply.github.com>
2024-08-17 21:22:18 +0530

    Connected to Convex db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,50 @@
 "use client"
 
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useConvex, useMutation, useQuery } from 'convex/react'
+import React, { useEffect } from 'react'
 
-function page() {
+
+function DashBoard() {
+
+  const convex=useConvex();
+  const {user}:any=useKindeBrowserClient();
+  const getUser=useQuery(api.user.getUser,{email:user?.email});
+
+  const createUser=useMutation(api.user.createUser);
+  useEffect(()=>{
+      if(user)
+      {
+        checkUser()
+      }
+  },[user])
+  
+
+  const checkUser=async()=>{
+    const result=await convex.query(api.user.getUser,{email:user?.email});
+    if(!result?.length)
+    {
+        createUser({
+          name:user.given_name,
+          email:user.email,
+          image:user.picture
+        }).then((resp)=>{
+          console.log(resp)
+        })
+    }
+
+  }
+
+
   return (
-    <div>DashBoard
+    
 <LogoutLink>
 <button>Logout</button>
 </LogoutLink>
-    </div>
+  
   )
 }   
 
-export default page
+export default DashBoard
